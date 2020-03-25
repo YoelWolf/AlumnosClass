ab7d5f50dc0552a38fca0626c352949d809b5239
YoelAguirreChaio <44403085+YoelWolf@users.noreply.github.com>
2020-03-25 13:45:52 -0300

    Add files via upload

---
+++ src/notasAlumnos/PrincipalNotas.java
@@ -0,0 +1,36 @@
+package notasAlumnos;
+
+public class PrincipalNotas {
+
+	
+	
+	public static void main(String[] args) {
+		
+			AlumnosNombres alumno1 = new AlumnosNombres("Alexander","T.Brown","001",7,6,8);
+			
+			alumno1.estadoAlumno();
+			
+			AlumnosNombres alumno2 = new AlumnosNombres("Marty","Mcfly","002",2,1,5);
+			
+			alumno2.estadoAlumno();
+   
+            AlumnosNombres alumno3 = new AlumnosNombres("Emmeth","Brown","003",6,4,2);
+            
+            alumno3.estadoAlumno();
+            
+            AlumnosNombres alumno4 = new AlumnosNombres("James","T.Kirk","004",9,9,8);
+            
+            alumno4.estadoAlumno();
+            
+            AlumnosNombres alumno5 = new AlumnosNombres("Alexander","Hartdegen","005",9,9,9);
+    
+            alumno5.estadoAlumno();
+            
+            AlumnosNombres alumno6 = new AlumnosNombres("Yoel","Aguirre Chaio","006",8,6,5);
+            
+            alumno6.estadoAlumno();
+            
+}
+
+}
+
